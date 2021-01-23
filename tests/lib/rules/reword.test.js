9e1f05ee0ce0b2fcf71d61e2896f644868c23973
yannickbuntsma <yannickbuntsma@gmail.com>
2021-01-23 17:48:03 +0100

    feat: casing overhaul, start testing

--- tests/lib/rules/reword.test.js
+++ tests/lib/rules/reword.test.js
@@ -1,8 +1,48 @@
-var assert = require('assert')
-describe('Array', function () {
-  describe('#indexOf()', function () {
-    it('should return -1 when the value is not present', function () {
-      assert.equal([1, 2, 3].indexOf(4), -1)
-    })
+const { RuleTester } = require('eslint')
+const rule = require('../../../lib/rules/reword')
+
+const ruleTester = new RuleTester({ parserOptions: { ecmaVersion: 2015 } })
+
+const createTitle = (title) => `reword | ${title}`
+const createDefaultErrorMessage = (match) =>
+  `Found match "${match}". This word is not allowed. Please reword accordingly.`
+
+;(() => {
+  const title = createTitle('basic partial word match')
+  const options = [['lemon']]
+  const errors = [{ message: createDefaultErrorMessage('lemonTree') }]
+
+  ruleTester.run(title, rule, {
+    valid: [
+      {
+        code: "const appleTree = '___'",
+        options,
+      },
+      {
+        code: 'function appleTree() {}',
+        options,
+      },
+      {
+        code: 'function doThing(appleTree) {}',
+        options,
+      },
+    ],
+    invalid: [
+      {
+        code: "const lemonTree = '___'",
+        options,
+        errors,
+      },
+      {
+        code: 'function lemonTree() {}',
+        options,
+        errors,
+      },
+      {
+        code: 'function doThing(lemonTree) {}',
+        options,
+        errors,
+      },
+    ],
   })
-})
+})()
